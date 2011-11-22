f8d5a71c947c67e3febc902b5139bdd83d066580
John Whitley <john@luminous-studios.com>
2011-11-22 14:15:36 -0800

    Update to RequireJS 1.0.2 release.
    Update require.js and add basic version/sanity test via ExecJS.

--- test/requirejs-rails_test.rb
+++ test/requirejs-rails_test.rb
@@ -1,8 +1,17 @@
 require 'test_helper'
 
+require 'execjs'
+require 'pathname'
+
 class RequirejsRailsTest < ActiveSupport::TestCase
   test "truth" do
     assert_kind_of Module, Requirejs
     assert_kind_of Module, Requirejs::Rails
+  end
+  
+  test "require.js version" do
+    require_js = Pathname.new(__FILE__+'/../../vendor/assets/javascripts/require.js').cleanpath.read
+    context = ExecJS.compile(require_js)
+    assert_equal "1.0.2", context.eval("require.version")
   end
 end
