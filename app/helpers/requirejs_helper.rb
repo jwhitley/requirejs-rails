937682e5a341e61543d4cd3bccc5f0d12c0a8fec
John Whitley <john@luminous-studios.com>
2012-04-24 14:53:15 -0700

    Guard existence of #controller in requirejs_include_tag
    Minor refactoring to aid some experiments in using test frameworks with the gem.

--- app/helpers/requirejs_helper.rb
+++ app/helpers/requirejs_helper.rb
@@ -33,34 +33,42 @@ module RequirejsHelper
 
     html = ""
 
-    if controller.requirejs_included
+    _once_guard do
+      unless requirejs.run_config.empty?
+        run_config = requirejs.run_config
+        if Rails.application.config.assets.digest
+          modules = requirejs.build_config['modules'].map { |m| requirejs.module_name_for m }
+
+          # Generate digestified paths from the modules spec
+          paths = {}
+          modules.each { |m| paths[m] = javascript_path(m).sub /\.js$/,'' }
+
+          # Override uesr paths, whose mappings are only relevant in dev mode
+          # and in the build_config.
+          run_config['paths'] = paths
+        end
+        html.concat <<-HTML
+        <script>var require = #{run_config.to_json};</script>
+        HTML
+      end
+
+      html.concat <<-HTML
+      <script #{_requirejs_data(name, &block)} src="#{javascript_path 'require.js'}"></script>
+      HTML
+
+      html.html_safe
+    end
+  end
+
+  def _once_guard
+    if defined?(controller) && controller.requirejs_included
       raise Requirejs::MultipleIncludeError, "Only one requirejs_include_tag allowed per page."
     end
 
-    unless requirejs.run_config.empty?
-      run_config = requirejs.run_config
-      if Rails.application.config.assets.digest
-        modules = requirejs.build_config['modules'].map { |m| requirejs.module_name_for m }
+    retval = yield
 
-        # Generate digestified paths from the modules spec
-        paths = {}
-        modules.each { |m| paths[m] = javascript_path(m).sub /\.js$/,'' }
-
-        # Override uesr paths, whose mappings are only relevant in dev mode
-        # and in the build_config.
-        run_config['paths'] = paths
-      end
-      html.concat <<-HTML
-      <script>var require = #{run_config.to_json};</script>
-      HTML
-    end
-
-    html.concat <<-HTML
-    <script #{_requirejs_data(name, &block)} src="#{javascript_path 'require.js'}"></script>
-    HTML
-
-    controller.requirejs_included = true
-    html.html_safe
+    controller.requirejs_included = true if defined?(controller)
+    retval
   end
 
   def _almond_include_tag(name, &block)
