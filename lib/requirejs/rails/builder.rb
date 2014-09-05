09a45cd1cd17d3342d4e867f84a36a8fb21fcf42
Jon Wu <jon@paperculture.com>
2014-09-04 17:20:56 -0700

    Allow functions r.js config by removing surrounding quotes

--- lib/requirejs/rails/builder.rb
+++ lib/requirejs/rails/builder.rb
@@ -25,8 +25,12 @@ module Requirejs::Rails
 
     def generate_rjs_driver
       templ = Erubis::Eruby.new(@config.driver_template_path.read)
+      # Hack to allow functions in config by removing surrounding quotes
+      driver = templ.result(@config.get_binding).gsub(/"(function\(.*?\)\s*?{.*?}[\s\\n]*)"/) do |f|
+        eval(f).strip.delete("\n")
+      end
       @config.driver_path.open('w') do |f|
-        f.write(templ.result(@config.get_binding))
+        f.write(driver)
       end
     end
   end
