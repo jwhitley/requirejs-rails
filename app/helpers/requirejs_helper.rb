1d083a1c0a0a36c0b6f8deb9602b85354de4681d
Remy Bach <remy@bach.me.uk>
2014-10-03 11:17:48 +0100

    Update protocol relative regex

--- app/helpers/requirejs_helper.rb
+++ app/helpers/requirejs_helper.rb
@@ -49,7 +49,7 @@ module RequirejsHelper
           if run_config.has_key? 'paths'
             # Add paths for assets specified by full URL (on a CDN)
             run_config['paths'].each do |k, v|
-              paths[k] = v if v.is_a?(Array) || v =~ /^(https?:|\/\/)/
+              paths[k] = v if v.is_a?(Array) || v =~ /^(https?:)?\/\//
 
             end
           end
