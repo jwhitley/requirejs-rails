d02ff1b5b45f4ad49e2b91a847a1394ce05619c9
Roy Liu <carsomyr@gmail.com>
2016-11-14 16:19:31 -0500

    Check whether the `ActionView::Base#check_precompiled_asset` method exists for backwards compatibility

--- lib/requirejs/rails/view.rb
+++ lib/requirejs/rails/view.rb
@@ -2,7 +2,8 @@ module Requirejs
   module Rails
     class View < ::ActionView::Base
       # This allows requirejs-rails to serve up modules by their undigestified asset paths.
-      self.check_precompiled_asset = false
+      self.check_precompiled_asset = false \
+        if respond_to?(:check_precompiled_asset)
     end
   end
 end
